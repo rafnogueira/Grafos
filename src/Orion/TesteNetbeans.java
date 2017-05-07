35cd3d32f2128380ac44286c24fb01178d0d8632
Rafael Nogueira <silva.rafael_81@hotmail.com>
2017-05-07 00:25:45 -0300

    Arquivos Criados

---
+++ src/Orion/TesteNetbeans.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Orion;
+
+/**
+ *
+ * @author rafael
+ */
+public class TesteNetbeans {
+    
+    private String msg = "Classe gerada no netbeans";
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+    
+    
+    
+}
